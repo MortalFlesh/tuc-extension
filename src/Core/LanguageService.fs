d19b54481db567be57539287028e5cc01afb31d5
Petr Chromec <mortal_flesh@seznam.cz>
2020-10-06 17:11:04 +0200

    fixup! fixup! fixup! Modify build for tuc

--- src/Core/LanguageService.fs
+++ src/Core/LanguageService.fs
@@ -11,9 +11,72 @@ open Ionide.VSCode.Helpers
 open DTO
 open LanguageServer
 
+module Notifications =
+    type DocumentParsedEvent =
+        { fileName : string
+          version : float
+          /// BEWARE: Live object, might have changed since the parsing
+          document : TextDocument }
+
+
+    let onDocumentParsedEmitter = EventEmitter<DocumentParsedEvent>()
+    let onDocumentParsed = onDocumentParsedEmitter.event
+
+    let private tooltipRequestedEmitter = EventEmitter<Position>()
+    let tooltipRequested = tooltipRequestedEmitter.event
+
+    let mutable notifyWorkspaceHandler : Option<Choice<ProjectResult,ProjectLoadingResult,(string * ErrorData),string> -> unit> = None
+
 module LanguageService =
     let [<Literal>] private TucLanguageShortName = "tuc"
     let [<Literal>] private TucLanguageName = "TypedUseCase"
+
+    module Types =
+        type PlainNotification= { content: string }
+
+        type ConfigValue<'a> =
+        | UserSpecified of 'a
+        | Implied of 'a
+
+        type [<RequireQualifiedAccess>] FSACTargetRuntime =
+        | NET
+        | NetcoreFdd
+
+        /// Position in a text document expressed as zero-based line and zero-based character offset.
+        /// A position is between two characters like an ‘insert’ cursor in a editor.
+        type Position = {
+            /// Line position in a document (zero-based).
+            Line: int
+
+            /// Character offset on a line in a document (zero-based). Assuming that the line is
+            /// represented as a string, the `character` value represents the gap between the
+            /// `character` and `character + 1`.
+            ///
+            /// If the character value is greater than the line length it defaults back to the
+            /// line length.
+            Character: int
+        }
+
+        type DocumentUri = string
+
+        type TextDocumentIdentifier = {Uri: DocumentUri }
+
+        type TextDocumentPositionParams = {
+            TextDocument: TextDocumentIdentifier
+            Position: Position
+        }
+
+        type FileParams = {
+            Project: TextDocumentIdentifier
+        }
+
+        type WorkspaceLoadParms = {
+            TextDocuments: TextDocumentIdentifier[]
+        }
+
+        type HighlightingRequest = {FileName : string; }
+        type FSharpLiterateRequest = {FileName: string}
+        type FSharpPieplineHintsRequest = {FileName: string}
 
     let mutable client : LanguageClient option = None
 
